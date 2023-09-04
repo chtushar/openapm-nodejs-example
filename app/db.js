34641a40ee0923e4617b14dbb38b99515091c112
Tushar Choudhari <tushar@last9.io>
2023-09-04 15:45:56 +0530

    Disable SSL

--- app/db.js
+++ app/db.js
@@ -1,41 +1,44 @@
-const mysql = require('mysql2');
-const {Sequelize, DataTypes} = require('sequelize');
+const mysql = require("mysql2");
+const { Sequelize, DataTypes } = require("sequelize");
 // Change your SSL Options as required. These are defaults for PlanetScale.
 const dbConnOpts = {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: true
-    }
-  }
-}
+      rejectUnauthorized: true,
+    },
+  },
+};
 
-const dbURI = `mysql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
-const sequelize = new Sequelize(dbURI, dbConnOpts);
+const dbURI = `mysql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+const sequelize = new Sequelize(dbURI, {});
 
 // Declare a Order Model
-const Order = sequelize.define('Order', {
+const Order = sequelize.define("Order", {
   customerName: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   product: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   quantity: {
     type: DataTypes.INTEGER,
-    allowNull: false
-  }
+    allowNull: false,
+  },
 });
 
 // Sync the model with the database
-sequelize.sync().then(() => {
-  console.log('Database synced');
-}).catch(error => {
-  console.error('Database sync error:', error);
-});
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synced");
+  })
+  .catch((error) => {
+    console.error("Database sync error:", error);
+  });
 
 module.exports = {
-  Order: Order
-}
+  Order: Order,
+};
--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
-const {faker} = require('@faker-js/faker');
-const express = require('express');
-const {Order} = require('./db');
-const {OpenAPM} = require('@last9/openapm');
+const { faker } = require("@faker-js/faker");
+const express = require("express");
+const { Order } = require("./db");
+const { OpenAPM } = require("@last9/openapm");
 
 // Initialize openAPM
 const openapm = new OpenAPM();
 
 // pass mysql as string value to initialize DB monitoring specifically for MySQL only.
-openapm.instrument('mysql');
+openapm.instrument("mysql");
 
 const app = express();
 const port = 6080;
@@ -20,57 +20,55 @@ async function insertFakeData() {
   let customerName = faker.name.fullName();
   let product = faker.commerce.productName();
   let quantity = faker.number.int({
-    'min': 1,
-    'max': 100
+    min: 1,
+    max: 100,
   });
 
   const order = await Order.create({
     customerName: customerName,
     product: product,
-    quantity: quantity
+    quantity: quantity,
   });
 
   console.log("Data insertion complete.");
 
-  return order
+  return order;
 }
 
 // Create Order API
-app.post('/orders', async (req, res) => {
-  const {customerName, product, quantity} = req.body;
+app.post("/orders", async (req, res) => {
+  const { customerName, product, quantity } = req.body;
 
   try {
-    const order = insertFakeData()
-    res.status(201).json({message: 'Order stored', orderId: order.id});
+    const order = insertFakeData();
+    res.status(201).json({ message: "Order stored", orderId: order.id });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({message: 'An error occurred'});
+    console.error("Error:", error);
+    res.status(500).json({ message: "An error occurred" });
   }
 });
 
 // Delete Last Order API
-app.delete('/orders/last', async (req, res) => {
+app.delete("/orders/last", async (req, res) => {
   try {
-
     // await throttle()
     const lastOrder = await Order.findOne({
-      order: [['id']] // Find the order with the highest id (last order)
+      order: [["id"]], // Find the order with the highest id (last order)
     });
 
-
     if (!lastOrder) {
-      return res.status(404).json({message: 'No orders found'});
+      return res.status(404).json({ message: "No orders found" });
     }
 
     await lastOrder.destroy();
 
-    res.json({message: 'Last order deleted'});
+    res.json({ message: "Last order deleted" });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({message: 'An error occurred'});
+    console.error("Error:", error);
+    res.status(500).json({ message: "An error occurred" });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT ?? port, () => {
+  console.log(`Server is running on port ${process.env.PORT ?? port}`);
 });
